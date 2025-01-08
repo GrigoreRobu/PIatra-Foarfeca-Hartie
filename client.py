2174e0ab5e40ec46303b9cd8f3bf9e1c3f4c9390
GrigoreRobu <144011337+GrigoreRobu@users.noreply.github.com>
2025-01-08 16:16:00 +0200

    Am schimbat mai multe chestii x2(not tested idk dc dau push)

--- client.py
+++ client.py
@@ -3,7 +3,6 @@ import socket
 host = '127.0.0.1'
 port = 8888
 
-
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((host, port))
 
--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 import socket
 import random
 
-
 host = '127.0.0.1'
 port = 8888
 
@@ -25,12 +24,15 @@ def game_start(client_socket):
     player_score = 0
     computer_score = 0
     rounds = 3
+
     while player_score < 2 and computer_score < 2:
-        client_socket.send("Alege varianta (P/H/F): ".encode())
+        client_socket.send("Serverul a generat o valoare random. Este pregatit!\n".encode())
+
+        client_socket.send("Alege varianta (P pentru Piatra, H pentru Hartie, F pentru Foarfeca): ".encode())
         player_choice = client_socket.recv(1024).decode().strip().upper()
 
         if player_choice not in ['P', 'H', 'F']:
-            client_socket.send("Nu există o astfel de variantă. Verificați și introduceți din nou.\n".encode())
+            client_socket.send("Nu exista o astfel de varianta. Verificati si introduceti din nou.\n".encode())
             continue
 
         computer_choice = get_computer_choice()
@@ -47,7 +49,7 @@ def game_start(client_socket):
         client_socket.send(f"Scor: Tu {player_score} - {computer_score} Computerul\n".encode())
 
     if player_score > computer_score:
-        client_socket.send(f"Ai câștigat din {player_score} încercări!\n".encode())
+        client_socket.send(f"Ai castigat din {player_score} incercari!\n".encode())
     elif player_score == computer_score:
         client_socket.send("Remiza!\n".encode())
     else:
@@ -57,14 +59,15 @@ def game_start(client_socket):
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((host, port))
 server_socket.listen(2)
-print('Serverul TCP este pregătit de lucru și ascultă pe adresa:', server_socket.getsockname())
+print('Serverul TCP este pregatit de lucru si asculta pe adresa:', server_socket.getsockname())
 
 while True:
     client_socket, addr = server_socket.accept()
     print(f"Adresa {addr} s-a conectat.")
 
     while True:
-        client_socket.send("Scrie START pentru a începe jocul de Piatra Foarfeca Hartie sau Q pentru a ieși.\n".encode())
+        client_socket.send(
+            "Scrie START pentru a incepe jocul de Piatra Foarfeca Hartie sau Q pentru a iesi.\n".encode())
         start_command = client_socket.recv(1024).decode().strip().upper()
         if start_command == "START":
             game_start(client_socket)
@@ -72,5 +75,6 @@ while True:
             client_socket.send("Deconectare...\n".encode())
             break
         else:
-            client_socket.send("Comandă necunoscută. Vă rog să introduceți START pentru a începe jocul sau Q pentru a ieși.\n".encode())
+            client_socket.send(
+                "Comanda necunoscuta. Va rog sa introduceti START pentru a incepe jocul sau Q pentru a iesi.\n".encode())
     client_socket.close()
